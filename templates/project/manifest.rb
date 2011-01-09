08469bfe577cb3b684019edbf64726d5abc941b0
evenwu <evendesign@gmail.com>
2011-01-10 02:45:21 +0800

    add README

---
+++ templates/project/manifest.rb
@@ -0,0 +1,15 @@
+# Make sure you list all the project template files here in the manifest.
+stylesheet 'main.scss', :media => 'screen'
+html 'simple.html'
+
+description "Simple 2 column layout generator"
+
+help %Q{
+Installs some html and a stylesheet that you can use directly
+or refer to as an example.
+}
+
+welcome_message %Q{
+Please refer to the simple.html file to see how the markup should be structured.
+And refer to the main.scss to see how to use the extension and apply it to your markup.
+}
